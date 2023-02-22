f08b7c23630d0c1011b81d697335774ee6c57be8
Sanuthabdulmumin02082001 <sanuthabdulmumin@gmail.com.com>
2023-02-22 19:04:21 +0100

    some additional fixes

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,8 +12,14 @@ if (n <= 98)
 {
 for (i = n; i <= 98; i++)
 {
-if (i != 98)printf("%d, ", i);
-else if (i == 98)printf("%d\n", i);
+if (i != 98)
+{
+printf("%d, ", i);
+}
+else if (i == 98)
+{
+printf("%d\n", i);
+}
 }
 }
 else if (n >= 98)
