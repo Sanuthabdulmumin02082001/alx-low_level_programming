0e50106be776a98591a76a1b9b651a08eb3c2fb9
Sanuthabdulmumin02082001 <sanuthabdulmumin@gmail.com.com>
2023-04-11 22:44:14 +0100

    Completed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,23 +5,22 @@
  */
 void print_binary(unsigned long int n)
 {
-    int shift;
-    unsigned long int temp;
+int shift;
+unsigned long int temp;
+if (n == 0)
+{
+_putchar('0');
+return;
+}
 
-    if (n == 0)
-    {
-        _putchar('0');
-        return;
-    }
-
-    for (temp = n, shift = 0; (temp >>= 1) > 0; shift++)
-        ;
-    for (; shift >= 0; shift--)
-    {
-        if ((n >> shift) & 1)
-            _putchar('1');
-        else
-            _putchar('0');
-    }
+for (temp = n, shift = 0; (temp >>= 1) > 0; shift++)
+;
+for (; shift >= 0; shift--)
+{
+if ((n >> shift) & 1)
+_putchar('1');
+else
+_putchar('0');
+}
 
 }
